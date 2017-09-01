fffbc0688aa8cdf1dfd1ee576f323cb33f398f03
eulernunez <eulernunez@gmail.com>
2017-09-01 09:13:32 +0200

    New implementation

---
+++ module/Inventario/src/Inventario/Model/Entity/BackupEquipo.php
@@ -0,0 +1,203 @@
+<?php
+/**
+ * Description of Equipo
+ * @author Euler Nuñez
+ */
+
+// module/Inventario/src/Inventario/Model/Entity/Equipo.php
+
+namespace Inventario\Model\Entity;
+
+class Equipo {
+
+    protected $id;
+    protected $eservicio;
+    protected $enemonico;
+    protected $eipgestion;
+    protected $enivel;
+    protected $enemonicon1;
+    protected $efabricante;
+    protected $emodelo;
+    protected $eserie;
+    protected $eubicacion;
+    protected $elogosalta;
+    protected $eestado;
+    protected $eobservacion;
+
+    protected $contactoId;
+    protected $circuitoId;
+    
+    public function __construct(array $options = null) {
+        
+        if (is_array($options)) {
+            $this->setOptions($options);
+        }
+    }
+
+    public function __set($name, $value) {
+        $method = 'set' . $name;
+        if (!method_exists($this, $method)) {
+            throw new Exception('Invalid Method');
+        }
+        $this->$method($value);
+    }
+
+    public function __get($name) {
+        $method = 'get' . $name;
+        if (!method_exists($this, $method)) {
+            throw new Exception('Invalid Method');
+        }
+        return $this->$method();
+    }
+
+    public function setOptions(array $options) {
+        $methods = get_class_methods($this);
+        foreach ($options as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            if (in_array($method, $methods)) {
+                $this->$method($value);
+            }
+        }
+        return $this;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getEservicio() {
+        return $this->eservicio;
+    }
+
+    public function setEservicio($eservicio) {
+        $this->eservicio = $eservicio;
+        return $this;
+    }
+    
+    public function getEnemonico() {
+        return $this->enemonico;
+    }
+
+    public function setEnemonico($enemonico) {
+        $this->enemonico = $enemonico;
+        return $this;
+    }
+
+    public function getEipgestion() {
+        return $this->eipgestion;
+    }
+
+    public function setEipgestion($eipgestion) {
+        $this->eipgestion = $eipgestion;
+        return $this;
+    }
+    
+    public function getEnivel() {
+        return $this->enivel;
+    }
+
+    public function setEnivel($enivel) {
+        $this->enivel = $enivel;
+        return $this;
+    }
+    
+    public function getEnemonicon1() {
+        return $this->enemonicon1;
+    }
+
+    public function setEnemonicon1($enemonicon1) {
+        $this->enemonicon1 = $enemonicon1;
+        return $this;
+    }
+    
+    public function getEfabricante() {
+        return $this->efabricante;
+    }
+
+    public function setEfabricante($efabricante) {
+        $this->efabricante = $efabricante;
+        return $this;
+    }
+
+    public function getEmodelo() {
+        return $this->emodelo;
+    }
+
+    public function setEmodelo($emodelo) {
+        $this->emodelo = $emodelo;
+        return $this;
+    }
+    
+    public function getEserie() {
+        return $this->eserie;
+    }
+
+    public function setEserie($eserie) {
+        $this->eserie = $eserie;
+        return $this;
+    }
+    
+    public function getEubicacion() {
+        return $this->eubicacion;
+    }
+
+    public function setEubicacion($eubicacion) {
+        $this->eubicacion = $eubicacion;
+        return $this;
+    }
+    
+    public function getElogosalta() {
+        return $this->elogosalta;
+    }
+
+    public function setElogosalta($elogosalta) {
+        $this->elogosalta = $elogosalta;
+        return $this;
+    }
+
+    public function getEestado() {
+        return $this->eestado;
+    }
+
+    public function setEestado($eestado) {
+        $this->eestado = $eestado;
+        return $this;
+    }
+
+    public function getEobservacion() {
+        return $this->eobservacion;
+    }
+
+    public function setEobservacion($eobservacion) {
+        $this->eobservacion = $eobservacion;
+        return $this;
+    }
+
+    public function getContactoId() {
+        return $this->contactoId;
+    }
+
+    public function setContactoId($contactoId) {
+        $this->contactoId = $contactoId;
+        return $this;
+    }
+
+    public function getCircuitoId() {
+        return $this->circuitoId;
+    }
+
+    public function setCircuitoId($circuitoId) {
+        $this->circuitoId = $circuitoId;
+        return $this;
+    }
+
+
+
+    
+}
+
---
+++ module/Inventario/src/Inventario/Model/Entity/IpLan.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * Description of IP WAN
+ * @author Euler Nuñez
+ */
+
+// module/Inventario/src/Inventario/Model/Entity/IpWan.php
+
+namespace Inventario\Model\Entity;
+
+class IpWan {
+
+    protected $id;
+    
+    protected $ipwrpv;
+    protected $ipwrouting;
+    protected $ipwvlanedc;
+    protected $ipwvlannacional;
+    protected $ipwred;
+    protected $ipwuso;
+    protected $ipwipwanedc;
+    protected $ipwmascara;
+    protected $ipwpeppal;
+    protected $ipwpebackup;
+
+    protected $equipoId;
+    
+    public function __construct(array $options = null) {
+        
+        if (is_array($options)) {
+            $this->setOptions($options);
+        }
+    }
+
+    public function __set($name, $value) {
+        $method = 'set' . $name;
+        if (!method_exists($this, $method)) {
+            throw new Exception('Invalid Method');
+        }
+        $this->$method($value);
+    }
+
+    public function __get($name) {
+        $method = 'get' . $name;
+        if (!method_exists($this, $method)) {
+            throw new Exception('Invalid Method');
+        }
+        return $this->$method();
+    }
+
+    public function setOptions(array $options) {
+        $methods = get_class_methods($this);
+        foreach ($options as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            if (in_array($method, $methods)) {
+                $this->$method($value);
+            }
+        }
+        return $this;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getIpwrpv() {
+        return $this->ipwrpv;
+    }
+
+    public function setIpwrpv($ipwrpv) {
+        $this->ipwrpv = $ipwrpv;
+        return $this;
+    }
+    
+    public function getIpwrouting() {
+        return $this->ipwrouting;
+    }
+
+    public function setIpwrouting($ipwrouting) {
+        $this->ipwrouting = $ipwrouting;
+        return $this;
+    }
+    
+    public function getIpwvlanedc() {
+        return $this->ipwvlanedc;
+    }
+
+    public function setIpwvlanedc($ipwvlanedc) {
+        $this->ipwvlanedc = $ipwvlanedc;
+        return $this;
+    }
+    
+    public function getIpwvlannacional() {
+        return $this->ipwvlannacional;
+    }
+
+    public function setIpwvlannacional($ipwvlannacional) {
+        $this->ipwvlannacional = $ipwvlannacional;
+        return $this;
+    }
+
+    public function getIpwred() {
+        return $this->ipwred;
+    }
+
+    public function setIpwred($ipwred) {
+        $this->ipwred = $ipwred;
+        return $this;
+    }
+
+    public function getIpwuso() {
+        return $this->ipwuso;
+    }
+
+    public function setIpwuso($ipwuso) {
+        $this->ipwuso = $ipwuso;
+        return $this;
+    }
+
+    public function getIpwipwanedc() {
+        return $this->ipwipwanedc;
+    }
+
+    public function setIpwipwanedc($ipwipwanedc) {
+        $this->ipwipwanedc = $ipwipwanedc;
+        return $this;
+    }
+    
+    public function getIpwmascara() {
+        return $this->ipwmascara;
+    }
+
+    public function setIpwmascara($ipwmascara) {
+        $this->ipwmascara = $ipwmascara;
+        return $this;
+    }
+
+    public function getIpwpeppal() {
+        return $this->ipwpeppal;
+    }
+
+    public function setIpwpeppal($ipwpeppal) {
+        $this->ipwpeppal = $ipwpeppal;
+        return $this;
+    }
+    
+    public function getIpwpebackup() {
+        return $this->ipwpebackup;
+    }
+
+    public function setIpwpebackup($ipwpebackup) {
+        $this->ipwpebackup = $ipwpebackup;
+        return $this;
+    }
+
+    public function getEquipoId() {
+        return $this->equipoId;
+    }
+
+    public function setEquipoId($equipoId) {
+        $this->equipoId = $equipoId;
+        return $this;
+    }
+    
+}
+
