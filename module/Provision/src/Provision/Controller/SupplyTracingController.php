9acce2b8fd7ae3ea127e8ae52d919dee93da1a6e
3ur3ka <w3b.d3v3lop3r.3ul3r@gmail.com>
2020-03-10 11:51:16 +0100

    Login process fixed.

--- module/Provision/src/Provision/Controller/SupplyTracingController.php
+++ module/Provision/src/Provision/Controller/SupplyTracingController.php
@@ -11,6 +11,9 @@ use Provision\Form\Supply;
 use Zend\View\Model\ViewModel;
 use Zend\Db\Adapter\Driver\Pdo\Resul;
 
+# HACK TO RESOLVE PROBLEM
+use Zend\Session\Container;
+
 
 class SupplyTracingController extends AbstractActionController
 {
@@ -20,6 +23,25 @@ class SupplyTracingController extends AbstractActionController
     public function setSupplyTracingService($service) {
         $this->supplyTracingService = $service;
         return $this;
+    }
+    
+    protected function attachDefaultListeners() {
+
+        parent::attachDefaultListeners();
+        $events = $this->getEventManager();
+        $events->attach('dispatch', array($this, 'preDispatch'), 50);
+        //$events->attach('dispatch', array($this, 'postDispatch'), -100);
+
+    }
+
+    public function preDispatch () {
+        # HACK TO RESOLVE PROBLEM
+        $session = new Container('User');
+        if(empty($session->offsetGet('userRole'))&&empty($session->offsetGet('firstName'))) {
+            return $this->redirect()->toRoute('users',
+                array('controller'=> 'User',
+                      'action' => 'logout'));
+        }
     }
     
     public function indexAction()
